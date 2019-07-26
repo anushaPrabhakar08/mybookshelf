1652d72261a4db3fff8367279005c09a4782a5ae
Anusha Prabhakar <51149041+anushaPrabhakar08@users.noreply.github.com>
2019-07-26 10:26:45 +0530

    Add files via upload

---
+++ src/search/SearchBooks.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+//import * as BooksAPI from '../../utils/BooksAPI'
+//import * as Common from '../../utils/Common'
+//import Autocomplete from '../../Autocomplete/Autocomplete'
+//import Info from '../Notification/Info'
+import BookShelfContainer from '../BookShelf/BookShelfContainer'
+
+class SearchBooks extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+     
+    }
+  }
+
+  
+
+  render() {
+    
+    return (
+      <div>
+        <h1>search</h1>
+      </div>
+      
+    )
+  }
+}
+
+export default SearchBooks
